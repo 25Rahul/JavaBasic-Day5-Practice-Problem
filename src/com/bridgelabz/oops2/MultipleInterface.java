cd5fce4f153596a982a83e9daeef6e16fa6ea113
Rahul Gupta <111953769+25Rahul@users.noreply.github.com>
2024-02-15 12:07:49 +0530

    Add files via upload

--- src/com/bridgelabz/oops2/MultipleInterface.java
+++ src/com/bridgelabz/oops2/MultipleInterface.java
@@ -1,2 +1,51 @@
-package com.bridgelabz.oops2;public class MultipleInterface {
+package com.bridgelabz.oops2;
+
+class Phone{
+    void voiceCall(){
+        System.out.println("Make VoiceClass");
+    }
+    void sms(){
+        System.out.println("We Can send SMS");
+    }
+}
+interface Camera{
+    void click();
+    void record();
+}
+
+interface Player{
+    void play();
+    void pause();
+    void stop();
+}
+
+class Smartphone extends Phone implements Camera,Player{
+
+    public void click() {
+        System.out.println("Take a Selfi");
+    }
+    public void record() {
+        System.out.println("Take a video");
+    }
+    public void play() {
+        System.out.println("Play Music");
+    }
+    public void pause() {
+        System.out.println("Pause Music");
+    }
+    public void stop() {
+        System.out.println("Stop Music");
+    }
+}
+public class MultipleInterface {
+    public static void main(String[] args) {
+        Smartphone oppo = new Smartphone();
+        oppo.voiceCall();
+        oppo.sms();
+        oppo.click();
+        oppo.record();
+        oppo.play();
+        oppo.pause();
+        oppo.stop();
+    }
 }
