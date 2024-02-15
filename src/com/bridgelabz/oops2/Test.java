8d5bf88547b72d0930bd9df75a9c2fce67df2921
Rahul Gupta <111953769+25Rahul@users.noreply.github.com>
2024-02-15 12:08:31 +0530

    Add files via upload

--- src/com/bridgelabz/oops2/Test.java
+++ src/com/bridgelabz/oops2/Test.java
@@ -1,2 +1,21 @@
-package com.bridgelabz.oops2;public class Test {
+package com.bridgelabz.oops2;
+interface A<T>
+{
+    public void m1(T t);
+}
+public class Test <T> implements A<T>{
+    public void m1(T t)
+    {
+        System.out.println("m1-Test");
+    }
+    public static void main(String[] args)
+    {
+
+        Test<Integer> t1 = new Test<Integer>();
+        t1.m1(25);
+
+        Test<String> t2 = new Test<String>();
+        t2.m1("John");
+    }
+
 }
